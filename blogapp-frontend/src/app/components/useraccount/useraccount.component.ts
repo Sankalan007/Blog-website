9cbc04b0b094fdad715dc71f44ae1fcdbdcc546e
Sankalan007 <schanda@argusoft.com>
2023-04-04 18:38:11 +0530

    Updated user details page

--- blogapp-frontend/src/app/components/useraccount/useraccount.component.ts
+++ blogapp-frontend/src/app/components/useraccount/useraccount.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import User from 'src/app/model/User';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-useraccount',
   templateUrl: './useraccount.component.html',
-  styleUrls: ['./useraccount.component.css']
+  styleUrls: ['./useraccount.component.css'],
 })
-export class UseraccountComponent {
+export class UseraccountComponent implements OnInit {
+  userDetails!: any;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  getUser() {
+    this.authService.getUserDetails().subscribe(
+      (res) => {
+        this.userDetails = res;
+      },
+      (error) => {
+        alert(error.message);
+      }
+    );
+  }
 }
